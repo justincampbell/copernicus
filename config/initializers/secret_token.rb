b036eb9269a7163d60558f021341aad3059a8efc
Justin Campbell <justin@justincampbell.me>
2012-10-01 13:39:04 -0400

    rails new Copernicus

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Copernicus::Application.config.secret_token = '79958601ff922a40f32188671fa103f2be482afe177c3e0b0df163f53ff6e531c44ab86b40a6d57e37858c6ce6d6e329dc05953adc7020f521659c8db8eaf24e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Copernicus::Application.config.session_store :cookie_store, key: '_Copernicus_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Copernicus::Application.config.session_store :active_record_store
