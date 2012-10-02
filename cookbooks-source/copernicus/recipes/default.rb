007fe4ab49fe32171ca494d9d8f0e61b3a736357
Justin Campbell <justin@justincampbell.me>
2012-10-02 17:10:49 -0400

    Basic copernicus recipe

---
+++ cookbooks-source/copernicus/recipes/default.rb
@@ -0,0 +1,51 @@
+#
+# Cookbook Name:: copernicus
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+
+include_recipe "apt"
+include_recipe "build-essential"
+include_recipe "git"
+
+node['rvm']['user_installs'] = [
+  {
+    'user'          => 'vagrant',
+    'default_ruby'  => '1.9.3',
+    'rubies'        => ['1.9.3']
+  }
+]
+
+include_recipe "rvm::user"
+
+directory "/srv" do
+  recursive true
+  owner "vagrant"
+  group "vagrant"
+  mode "0755"
+end
+
+execute "git clone git://github.com/JustinCampbell/copernicus.git /srv/copernicus" do
+  not_if { File.exists? '/srv/copernicus' }
+  user "vagrant"
+end
+
+execute "git pull" do
+  cwd "/srv/copernicus"
+  user "vagrant"
+end
+
+rvm_shell "install gems" do
+  code "bundle install"
+  cwd "/srv/copernicus"
+  user "vagrant"
+end
+
+rvm_shell "start server" do
+  code "bundle exec rails server --daemon --port 8080"
+  cwd "/srv/copernicus"
+  user "vagrant"
+end
+
